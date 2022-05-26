da10d890520cf6c6c3366be7554543ef541bf220
Jehyun Jung <toojey7@gmail.com>
2022-05-26 17:06:41 +0900

    fin: final commit

--- js/background.js
+++ js/background.js
@@ -1,7 +1,3 @@
 const images=["img1.jpg","img2.jpg","img3.jpg"];
 const chosenImage=images[Math.floor(Math.random()*images.length)];
-
-const bgImage=document.createElement("img");
-bgImage.src=`img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage=`url("img/${chosenImage}")`;
--- js/weather.js
+++ js/weather.js
@@ -5,6 +5,7 @@ function onGeoOk(position){
     const lng=position.coords.longitude;
     console.log("you live in ",lat,lng);
     const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    
     fetch(url)
         .then(response => response.json())
         .then(data => { 
